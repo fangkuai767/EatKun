fb09d07e963986a5dd7666fceaef93bb408771cf
fangkuai767 <67937954+fangkuai767@users.noreply.github.com>
2022-01-31 12:40:57 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -343,11 +343,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 30) return '拜托你逊爆了欸';
-    if (score <= 60) return '勉勉强强';
-    if (score <= 100) return '得到老八真传了？';
-    if (score <= 150) return '老八附体';
-    return '你就是老八本人？';
+    if (score <= 30) return '鸡你太美';
+    if (score <= 60) return '鸡你太太美';
+    if (score <= 100) return '鸡鸡鸡鸡鸡';
+    if (score <= 150) return '唱 跳 rap 篮球';
+    return '你就是蔡徐坤本人？';
 }
 
 function toStr(obj) {
