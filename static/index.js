7165898675598d707f101ef25356b0a2fe910bf1
hanxindaijinhua <105840066+hanxindaijinhua@users.noreply.github.com>
2022-05-19 14:20:37 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 let isDesktop = navigator['userAgent'].match(/(ipad|iphone|ipod|android|windows phone)/i) ? false : true;
-let fontunit = isDesktop ? 20 : ((window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth) / 320) * 10;
+let fontunit = isDesktop ? 99999 : ((window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth) / 320) * 10;
 document.write('<style type="text/css">' +
     'html,body {font-size:' + (fontunit < 30 ? fontunit : '30') + 'px;}' +
     (isDesktop ? '#welcome,#GameTimeLayer,#GameLayerBG,#GameScoreLayer.SHADE{position: absolute;}' :
